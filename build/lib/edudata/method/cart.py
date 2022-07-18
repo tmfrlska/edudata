a606ffc1fd99158e8f2d5fa6d000f83f91cedbf1
tmfrlska <tmfrlska85@gmail.com>
2022-07-18 17:06:33 +0900

    add_csv

---
+++ build/lib/edudata/method/cart.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+from edudata.method import Method, proper, smooth
+from edudata import NUM_COLS_DTYPES, CAT_COLS_DTYPES
+
+class CARTMethod(Method):
+    def __init__(self, dtype, smoothing=False, proper=False, minibucket=5, random_state=None, *args, **kwargs):
+        self.dtype = dtype
+        self.smoothing = smoothing
+        self.proper = proper
+        self.minibucket = minibucket
+        self.random_state = random_state
+
+        if self.dtype in CAT_COLS_DTYPES:
+            self.cart = DecisionTreeClassifier(min_samples_leaf=self.minibucket, random_state=self.random_state)
+        if self.dtype in NUM_COLS_DTYPES:
+            self.cart = DecisionTreeRegressor(min_samples_leaf=self.minibucket, random_state=self.random_state)
+
+    def fit(self, X_df, y_df, random_state=None):
+        if self.proper:
+            X_df, y_df = proper(X_df=X_df, y_df=y_df, random_state=self.random_state)
+
+        X_df, y_df = self.prepare_dfs(X_df=X_df, y_df=y_df, normalise_num_cols=False, one_hot_cat_cols=True)
+        if self.dtype in NUM_COLS_DTYPES:
+            self.y_real_min, self.y_real_max = np.min(y_df), np.max(y_df)
+
+        X = X_df.to_numpy()
+        y = y_df.to_numpy()
+        self.cart.fit(X, y)
+
+        leaves = self.cart.apply(X)
+        leaves_y_df = pd.DataFrame({'leaves': leaves, 'y': y})
+        self.leaves_y_dict = leaves_y_df.groupby('leaves').apply(lambda x: x.to_numpy()[:, -1]).to_dict()
+
+    def predict(self, X_test_df):
+        X_test_df, _ = self.prepare_dfs(X_df=X_test_df, normalise_num_cols=False, one_hot_cat_cols=True, fit=False)
+
+        # predict the leaves and for each leaf randomly sample from the observed values
+        X_test = X_test_df.to_numpy()
+        leaves_pred = self.cart.apply(X_test)
+        y_pred = np.zeros(len(leaves_pred), dtype=object)
+
+        leaves_pred_index_df = pd.DataFrame({'leaves_pred': leaves_pred, 'index': range(len(leaves_pred))})
+        leaves_pred_index_dict = leaves_pred_index_df.groupby('leaves_pred').apply(lambda x: x.to_numpy()[:, -1]).to_dict()
+        for leaf, indices in leaves_pred_index_dict.items():
+            np.random.seed(self.random_state)
+            y_pred[indices] = np.random.choice(self.leaves_y_dict[leaf], size=len(indices), replace=True)
+
+        if self.smoothing and self.dtype in NUM_COLS_DTYPES:
+            y_pred = smooth(self.dtype, y_pred, self.y_real_min, self.y_real_max, random_state=self.random_state)
+
+        return y_pred
--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 with open("VERSION", "r") as f:
-    version = f.read().strip()  
+    version = f.read().strip()
+
+install_requires = [
+    'numpy>=1.17.3',
+    'pandas>=0.25.3',
+    'scipy>=1.3.1',
+    'scikit-learn>=0.22',
+    'table_evaluator==1.4.2'
+    ]  
 
 setup(
     name="edudata4ai",
@@ -15,6 +23,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tmfrlska/edudata",
+    install_requires=install_requires,   
     packages=find_packages(),
     package_data={'': ['datasets/*.csv', 'datasets/*.json']},
     include_package_data=True,
